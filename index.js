4ed1a64ca54bf06a49810051eb4a95e9f664c7f3
sajib689 <jibonraja11@gmail.com>
2024-02-01 13:20:37 +0600

    f

---
+++ index.js
@@ -0,0 +1,9 @@
+const arr = ['Apples', 'Orange', 'Coffee', 'Maroon',]
+let nums = [1, 2, 3, 4, 5, 6]
+let sum = 0
+for (let i = 0; i < nums.length; i++){
+    const currentNumbers = nums[i]
+    sum += currentNumbers
+   
+}
+console.log(sum)
